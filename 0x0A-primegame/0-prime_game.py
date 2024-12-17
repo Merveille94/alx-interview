645684999839ffafae0a6de7119dfc5236cd5877
Merveille94 <emanueleguehi@gmail.com>
2024-12-17 00:48:39 +0000

    my second commit

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,48 @@
 #!/usr/bin/python3
 
-isWinner = __import__('0-prime_game').isWinner
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+def prime_count(n):
+    primes = [True] * (n + 1)
+    primes[0], primes[1] = False, False
+    count = 0
+
+    for i in range(2, n + 1):
+        if primes[i]:
+            count += 1
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+
+    return count
+
+
+def isWinner(x, nums):
+    if x <= 0 or not nums:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    max_n = max(nums)
+    prime_counts = [prime_count(i) for i in range(max_n + 1)]
+
+    for n in nums:
+        if prime_counts[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+
